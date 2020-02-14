2128e220680ee00b0b2aff4323a966ad93830eae
Chaney <52712847+chanbro@users.noreply.github.com>
2020-02-14 15:05:04 -0600

    added week 2 problems

--- src/Chaney2.java
+++ src/Chaney2.java
@@ -11,6 +11,8 @@ public class Chaney2 {
 
         System.out.println(getMax(data));
         System.out.println(getMin(data));
+        System.out.println(isKaprekar(34234));
+        System.out.println(move("computer"));
     }
 
     public static double getMax(double[][] numbers) {
@@ -49,5 +51,45 @@ public class Chaney2 {
             }
         }
         return minValue;
+
     }
-}
+
+//    A Kaprekar Number is a positive integer that, after being squared and split into two lexicographical parts, is equal to the sum of the two new numbers obtained:
+//
+//    If the quantity of digits of the squared number is even, the left and right parts will have the same length.
+//    If the quantity of digits of the squared number is odd, then the right part will be the longest half, with the left part being the smallest or equal to zero if the quantity of digits is equal to 1.
+//    Given a positive integer n implement a function that returns true if it's a Kaprekar number, and false if it's not.
+
+        public static boolean isKaprekar(long n) {
+            if (n <= 1) {
+                return true;
+            }
+            String sq = ("" + (n * n));
+            long n1 = (sq.length() == 1) ? 0 : Long.parseLong(sq.substring(0, sq.length()/2));
+            long n2 = Long.parseLong(sq.substring(sq.length()/2));
+            return n1 + n2 == n;
+        }
+
+//    Write a function that changes every letter to the next letter:
+//
+//            "a" becomes "b"
+//            "b" becomes "c"
+//            "d" becomes "e"
+//    and so on ...
+
+        public static String move(String word) {
+            String alpha = "abcdefghijklmnopqrstuvwxyz";
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                int index = alpha.indexOf(c);
+                sb.append(alpha.charAt(index+1));
+            }
+            return sb.toString();
+        }
+
+//    move("hello") ➞ "ifmmp"
+//
+//    move("bye") ➞ "czf"
+//
+//    move("welcome") ➞ "xfmdpnf"
+    }
