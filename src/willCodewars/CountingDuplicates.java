d07cca67700f22d33bc151a99fcbeeba304f9543
William Stephens <william.a.stephens17@gmail.com>
2020-02-21 13:46:52 -0600

    Update CountingDuplicates.java

--- src/willCodewars/CountingDuplicates.java
+++ src/willCodewars/CountingDuplicates.java
@@ -1,4 +1,22 @@
 package willCodewars;
 
-public class countingDuplicates {
+public class CountingDuplicates {
+    public static void main(String[] args) {
+        System.out.println(duplicateCount("How many duplicates?")); //2
+        System.out.println(duplicateCount("abcdABCDaa")); //4
+    }
+
+    public static int duplicateCount(String text) {
+        int ans = 0;
+        text = text.toLowerCase();
+        while (text.length() > 0) {
+            String firstLetter = text.substring(0,1);
+            text = text.substring(1);
+            if (text.contains(firstLetter)) {
+                ans ++;
+            }
+            text = text.replace(firstLetter, "");
+        }
+        return ans;
+    }
 }
