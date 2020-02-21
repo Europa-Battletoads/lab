b34c54a241fbdcf78b42e3008fa9a650775c711c
William Stephens <william.a.stephens17@gmail.com>
2020-02-21 13:46:13 -0600

    Update AreSame.java

--- src/willCodewars/AreSame.java
+++ src/willCodewars/AreSame.java
@@ -1,4 +1,29 @@
 package willCodewars;
 
+import java.util.Arrays;
+
 public class AreSame {
+    public static boolean comp(int[] a, int[] b) {
+        if(a == null || b == null) {
+            return false;
+        }
+
+        if(a.length != b.length) {
+            return false;
+        }
+
+        for(int i=0; i<b.length; ++i) {
+            a[i] = a[i]*a[i];
+        }
+
+        Arrays.sort(a); Arrays.sort(b);
+
+        for(int i=0; i<a.length; ++i) {
+            if(a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
